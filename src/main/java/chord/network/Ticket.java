6a9ae2c5df95bfda864a8f2b5c9acc2cae7613ba
arancicarini <37337897+arancicarini@users.noreply.github.com>
2019-04-17 19:00:06 +0200

    messageHandler

--- src/main/java/chord/network/Ticket.java
+++ src/main/java/chord/network/Ticket.java
@@ -6,6 +6,9 @@ public class Ticket {
     public synchronized static int getTicket(){
         //TO DO
         // add code to avoid overflow ( basically, restart ticket after a certain value)
+        if(ticket == Integer.MAX_VALUE){
+            ticket=0;
+        }
         ticket++;
         return ticket;
     }
