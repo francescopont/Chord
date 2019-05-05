801e2e95edf241d83d106bd03e0d231fa7b36a22
francescopont <37176513+francescopont@users.noreply.github.com>
2019-05-06 00:38:56 +0200

    FingerTableComparator

--- src/main/java/chord/model/FingerTableComparator.java
+++ src/main/java/chord/model/FingerTableComparator.java
@@ -11,10 +11,32 @@ public class FingerTableComparator implements Comparator<String> {
         this.nodeidentifier = nodeidentifier;
     }
 
+    //problema: quando gli passo il nodeidentifier che succede?
+    // ho fatto queste scelte di minori e uguali pensando alla successor list
     @Override
     public int compare(String hash1, String hash2){
-        //case1: both values are
+        //case1: both values are greater than the nodeidentifier
+        if (hash1.compareTo(nodeidentifier)>0 && hash2.compareTo(nodeidentifier) >0){
+            //we compare them as usual
+            return hash1.compareTo(hash2);
+        }
+        //case 2: the first value is smaller than nodeidentifier, the second is greater
+        if (hash1.compareTo(nodeidentifier)<=0 && hash2.compareTo(nodeidentifier) >0){
+            //the first one must be returned
+            return 1;
+        }
+        //case 3: the first value is greater than nodeidentifier, the second is smaller
+        if (hash1.compareTo(nodeidentifier) >0 && hash2.compareTo(nodeidentifier) <=0){
+            //the second one must be returned
+            return -1;
+        }
+        //case 4: both values are smaller than the nodeidentifier
+        if (hash1.compareTo(nodeidentifier)<=0 && hash2.compareTo(nodeidentifier) <=0){
+            //we compare them as usual
+            return hash1.compareTo(hash2);
+        }
+        //this instruction is never reached
+        return 0;
 
-        if (hash1.compareTo(nodeidentifier)>0 && hash2.compareTo(nodeidentifier) > 0)
     }
 }
