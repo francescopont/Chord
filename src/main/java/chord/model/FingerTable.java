2f3b5d64462d4f9952c82ef18585307cf1ab9860
arancicarini <37337897+arancicarini@users.noreply.github.com>
2019-05-06 15:52:21 +0200

    fingertable fix

--- src/main/java/chord/model/FingerTable.java
+++ src/main/java/chord/model/FingerTable.java
@@ -3,37 +3,32 @@ package chord.model;
 import java.util.*;
 
 public class FingerTable {
-    private List<String> fingers;
-    private HashMap<String, NodeInfo> finger_table;
-    private String nodeidentifier;
 
-    public FingerTable(String nodeidentifier){
-        this.finger_table=new HashMap<>();
-        this.fingers = new LinkedList<>();
-        this.nodeidentifier = nodeidentifier;
-        for (int i=1; i<17; i++){
-            String finger = Utilities.computefinger(nodeidentifier,i);
-            //the add method appends at the end of the list
-            fingers.add(finger);
-        }
+    private List<NodeInfo> finger_table;
+    private Comparator comparator;
+
+    public FingerTable(String nodeIdentifier){
+        this.finger_table=new LinkedList<>();
+        this.comparator= new FingerTableComparator(nodeIdentifier);
     }
 
-    public void addEntry(String finger, NodeInfo node){
-        finger_table.put(finger,node);
+    public void addEntry(NodeInfo node){
+        finger_table.add(node);
     }
 
-    public NodeInfo closestSuccessor(String key, String node){
-        //a set cancels duplicate elements ( which is a valuable behaviour)
-        for (
-        Set hashes = finger_table.keySet();
-        Collections.sort(hashes);
 
-        hashes,new NodeComparator);
-        for(int i=0; i<finger_table.size(); i++){
-           //trovo il closest
+    public NodeInfo closestSuccessor(NodeInfo node){
+        Collections.sort(finger_table,comparator);
+        NodeInfo predecessor=null;
+        for(NodeInfo nodeInfo: finger_table){
+           if (comparator.compare(nodeInfo,node)>=0){
+               return predecessor;
+           }
+           predecessor=nodeInfo;
         }
+        return predecessor;
         //da finire
-    }*/
+    }
 
 
 
