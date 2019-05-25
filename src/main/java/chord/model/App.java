ee9748c09d88682b49f38a5973022e477c539ed8
arancicarini <37337897+arancicarini@users.noreply.github.com>
2019-05-25 12:43:29 +0200

    bozza menu

--- src/main/java/chord/model/App.java
+++ src/main/java/chord/model/App.java
@@ -3,6 +3,7 @@ package chord.model;
 import chord.Exceptions.PortException;
 import chord.network.Router;
 
+import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -10,12 +11,47 @@ import java.util.TimerTask;
  * Hello world!
  *
  */
-public class App 
-{
-    //piccolo test
-    public static void main( String[] args )
-    {
+public class App
 
+{
+
+    //piccolo test
+    public static void main( String[] args ) {
+        Scanner scanner= new Scanner(System.in);
+        Scanner scanner1=new Scanner(System.in);
+
+        System.out.println("Selezionare un'opzione: \n c for Create \n j for Join ");{
+            String input= scanner.nextLine();
+            if(input.equals("c")){
+                System.out.println("Insert IP Address: ");
+                String ip=scanner.nextLine();
+                System.out.println("Insert port Number: ");
+                int port= scanner.nextInt();
+                try {
+                    Chord.create(ip,port);
+                } catch (PortException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            else if(input.equals("j")){
+                System.out.println("Insert your IP Address: ");
+                String ip=scanner.nextLine();
+                System.out.println("Insert the IP address of your friend: ");
+                String friendIp=scanner.nextLine();
+                System.out.println("Insert your port Number: ");
+                int port=scanner.nextInt();
+                System.out.println("Insert the port Number of your BFF: ");
+                int friendPort= scanner.nextInt();
+                try {
+                    Chord.join(ip,port,friendIp,friendPort);
+                } catch (PortException e) {
+                    e.printStackTrace();
+                }
+            }
+
+    }
+        /*
         int actual_port = 1000;
         try {
             Chord.create("127.0.0.1", 1000);
@@ -36,7 +72,7 @@ public class App
                 e.printStackTrace();
             }
         }
-
+        */
 
         System.out.println("finita la computazione");
         Timer timer=new Timer();
